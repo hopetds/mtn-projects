30f002ce6c88136d7751cbeebdc2bb3362055719
ilya_khamiakou <hopetds@gmail.com>
2017-02-22 16:45:23 +0300

    test

--- module3/ant-task/src/com/test/Project.java
+++ module3/ant-task/src/com/test/Project.java
@@ -6,10 +6,10 @@ import org.apache.commons.collections.Predicate;
 class Project {
 
   public static void test() {
-    System.out.println("test");
+    System.out.println("tested by Ilya_khamiakou");
   }
   
-  public static void main(String[] args) {
+  public static void main() {
     System.out.println("Hellow MTN");
   }
 
