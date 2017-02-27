ed6ffebc51053d9a66071f6fb691f806f2a9e8a8
hopetds <hopetds@gmail.com>
2017-02-27 15:19:09 +0300

    task-sonar

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -6,9 +6,19 @@ import org.apache.commons.collections.Predicate;
 class Project {
 
   public static void test() {
-    System.out.println("test");
+    System.out.println("Test comment ilya1");
   }
-  
+
+  public static void test () {
+	System.out.println("Test comment ilya2");
+	}
+  public static void test () {
+        System.out.println("Test comment ilya3");
+        } 
+  public static void test () {
+        System.out.println("Test comment ilya4");
+        } 
+
   public static void main(String[] args) {
     System.out.println("Hellow MTN");
   }
