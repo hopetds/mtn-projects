6cddfb222110f266d8e5aa0596549ad53ad7400d
Ilya Khamiakou <hopetds@gmail.com>
2017-05-30 16:53:02 +0300

    Update Test.java

--- module3/maven-simple/src/main/java/com/test/Test.java
+++ module3/maven-simple/src/main/java/com/test/Test.java
@@ -1,9 +1,8 @@
 package com.test;
 
 class Test {
-blablabla
   public static void main(String[] args) {
-    System.out.println("Hellow MTN and not hello MTN");
+    System.out.println("Helloasdasdasdas MTN and not hello MTN");
   }
 
 }
